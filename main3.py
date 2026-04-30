45ab3e43bd9d2e4f561573dffc8bef6e142e14c7
Osnolac <asuncion.ma@obmontessori.edu.ph>
2026-04-30 23:59:23 +0800

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,11 @@
+from pyscript import document
+
+document.getElementById("img1").src = "teachersdayimg.png"
+document.getElementById("img2").src = "christmasimg.png"
+document.getElementById("img3").src = "campimg.png"
+document.getElementById("img4").src = "speechchoirimg.png"
+document.getElementById("img5").src = "intramsimg.jpeg"
+document.getElementById("img6").src = "miscimg.jpg"
+
+
+
